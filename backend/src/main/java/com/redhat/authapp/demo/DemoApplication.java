8903ffcfe963584b3b49094d8c8479d29cb8d3c3
Abhishek Koserwal <akoserwa@redhat.com>
2018-02-15 17:33:54 +0530

    updated controller with cors annontation

--- backend/src/main/java/com/redhat/authapp/demo/DemoApplication.java
+++ backend/src/main/java/com/redhat/authapp/demo/DemoApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:4200")
+
+
 @SpringBootApplication
 public class DemoApplication {
 
--- backend/src/main/java/com/redhat/authapp/demo/controller/DemoController.java
+++ backend/src/main/java/com/redhat/authapp/demo/controller/DemoController.java
@@ -2,6 +2,7 @@ package com.redhat.authapp.demo.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.Map;
 /**
  * Created by akoserwa on 12/2/17.
  */
+@CrossOrigin(maxAge = 3600, origins = "http://localhost:4200")
 @RestController
 public class DemoController {
 
